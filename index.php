7db1427f1cf74f1756e20957246b28fa53c61c2c
Adam Evans <adamevans385@msn.com>
2017-08-06 21:18:52 +0100

    various changes
    various changes

--- index.php
+++ index.php
@@ -4,107 +4,84 @@
 <script>
  function tog(){
                 
-                alert('gets here!');
-                        $('#sideBarTog').toggle(); 
+//                alert('gets here!');
+                        $('#sideBarTog').show(); 
  }
+    function untog(){
+        $('#sideBarTog').hide();
+    }
+    
+    function bothTog(){
+        $('#sideBarTog').toggle();
+    }
 </script>
 
 <body>
-    <input type="button" onClick="tog()" value="button"/>
-<div class="container-fluid" name="main-content" id="main-content" ><!--style="border:solid; border-color:red;"-->
+    
+<div class="container-fluid" name="main-content" id="main-content" >
 
     
     
 <div class="row">
-
     
-    <div class="collapse navbar-collapse" id="sideBarTog">
+    <div class="col-sm-1 sidebar hidden-sm-down p-0" id="sideBarTog">
+        <div class="row">
+            
+            <div class="container-fluid p-3">
+                    <img src="images/placeholder.png" width="120px" height="120px">
+                
+            </div>
+            
         
-             
-            <nav class="nav nav-toggleable-md navbar-inverse flex-column p-1" name="sidebar" id="sideBar">
-                    
-                    <div class="col-sm-2 sidebar toggleable" >
-
-                    <a class="navbar-brand" href="#">Backoffice</a>
-                    
-                        <ul class="navbar-nav">
+        </div>            
+        
+                        <ul class="navbar-nav flex-column hidden-sm-down">
+                            
                             <li class="nav-item">
-                                <a class="nav-link " href="#">New Case<span class="sr-only">(current)</span></a>
+                                <a class="nav-link sidebarLink p-3" href="#">File<span class="sr-only">(current)</span></a>
                             </li>
-                            <li class="nav-item active ">
-                                <a class="nav-link " href="#">as</a>
+                            
+                            <li class="nav-item sidebarLink">
+                                <a class="nav-link sidebarLink p-3 active" href="#">Marketing</a>
                             </li>
                         
-                            <li class="nav-item ">
-                                <a class="nav-link " href="logout.html">Logout</a>
+                            <li class="nav-item sidebarLink">
+                                <a class="nav-link sidebarLink p-3" href="logout.html">Reports</a>
                             </li>
                         </ul>
                     
-                        
-                </nav>
 
             <!-- Page content goes here-->
             <div class="col"  name="pageContentContainer" id="pageContentContainer">
             
             </div>
-        </div>
+        
     </div>
     
-<!--
-       <nav class="nav  flex-column" id="sideBarNav">                                    
-                        <a class="nav-link"  href="#">Summary</a>
-                        <a class="nav-link" href="#" >Timeline</a>
-                        <a class="nav-link active" href="#">Product 
--->
-<!--    </div>-->
-<!--
-    <div class="row">
-        <div class="col-sm-2 sidebar" >
-            <nav class="navbar navbar-toggleable-md ">
-                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#sideBarTog" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <a class="navbar-brand" href="#">Options</a>
-                <div class="collapse navbar-collapse" id="sideBarTog">
-                    <ul class="nav flex-column">
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Files<span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Accounts<span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Marketing<span class="sr-only">(current)</span></a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">New Case<span class="sr-only">(current)</span></a>
-                        </li>
-                    </ul> 
-                </div>
-            </nav>
-        </div>
--->
-            
-        <div class="col-sm-10 p-0"> 
+
+        <div class="col p-0"> 
 
                 <nav class="navbar navbar-toggleable-md navbar-inverse bg-inverse topNavBar" name="topNavBar">
                     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#topNavBarTog" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span class="navbar-toggler-icon"></span>
                     </button>
-                    <button class="toggler nav-toggler-left myTog" type="button" data-toggle="collapse" data-target="#sideBarTog" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="z-index:1000;">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>   
-                    <a class="navbar-brand" href="#">Backoffice</a>
+                    <input type="button" class="hidden-sm-down"style="height:50px; margin-left:-20px;"  onClick="bothTog()" value=">"/>
+<!--                    <button class="toggler nav-toggler-left myTog" type="button" data-toggle="collapse" data-target="#sideBarTog" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation" style="z-index:1000;">-->
+<!--                        <span class="navbar-toggler-icon"></span>-->
+<!--                    </button>   -->
+                    <a class="navbar-brand" style="margin-left:10px;" href="#">Backoffice</a>
                     <div class="collapse navbar-collapse" id="topNavBarTog">
                         
                         
-                        <ul class="navbar-nav ">
+                        <ul class="navbar-nav">
                             <li class="nav-item ">
-                                <a class="nav-link " href="#">New Case<span class="sr-only">(current)</span></a>
+                                <a class="nav-link " href="#">System Settings<span class="sr-only"></span></a>
                             </li>
+<!--
                             <li class="nav-item active ">
-                                <a class="nav-link " href="#">as</a>
+                                <a class="nav-link " href="#"></a>
                             </li>
+-->
                         
                             <li class="nav-item ">
                                 <a class="nav-link " href="logout.html">Logout</a>
